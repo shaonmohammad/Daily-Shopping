37f4d1ff017ed9eebf9a87b6e23566670749c3cd
Shoan <shaonmohammad13@gmail.com>
2024-12-23 02:18:44 +0600

    solved static file issue

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 # Media files (Uploaded files like images)
